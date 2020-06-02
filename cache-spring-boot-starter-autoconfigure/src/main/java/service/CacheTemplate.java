1bece6fc26aa6adcdaa3cfbc74ca3fbdae8c5b92
acshmily <shmilytq@gmail.com>
2020-06-02 18:15:58 +0800

    init

--- cache-spring-boot-starter-autoconfigure/src/main/java/service/CacheTemplate.java
+++ cache-spring-boot-starter-autoconfigure/src/main/java/service/CacheTemplate.java
@@ -1,5 +1,10 @@
 package service;
 
+import org.springframework.boot.json.JsonParseException;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -52,7 +57,241 @@ public interface CacheTemplate {
      */
     void deleteHashKeys(String key,String ...hashKeys);
 
+    /**
+     * 判断key内是否存在指定hashKey
+     * @param key
+     * @param hashKey
+     * @return
+     */
+    Boolean hasHashKey(String key,String hashKey);
 
+    /**
+     * 按照key获取值并反序列化成实例返回
+     * @param key
+     * @param valueType
+     * @return
+     * @throws IOException
+     * @throws JsonParseException
+     */
+    <T> T get(String key,Class<T> valueType) throws IOException, JsonParseException;
+
+    /**
+     * 默认返回String值
+     * @param key
+     * @return
+     */
+    String get(String key);
+    /**
+     * 根据keykey及hashKey获取值并反序列化返回
+     * @param key
+     * @param hashKey
+     * @param valueType
+     * @return
+     * @throws IOException
+     * @throws JsonParseException
+     */
+    <T> T hashGet(String key,String hashKey,Class<T> valueType) throws IOException, JsonParseException;
+
+    /**
+     * 获取key及hashKey获取值并直接返回
+     * @param key
+     * @param hashKey
+     * @return
+     */
+    String hashGet(String key,String hashKey);
+
+    /**
+     * 根据key及hashKeys获取值返回并且反序列化
+     * @param key
+     * @param valueType
+     * @param hashKeys
+     * @return
+     * @throws IOException
+     * @throws JsonParseException
+     */
+    <T> List<T> hashMultiGet(String key, Class<T> valueType, String... hashKeys) throws IOException, JsonParseException;
+
+    /**
+     * 根据key及hashKeys返回数组
+     * @param key
+     * @param hashKeys
+     * @return
+     */
+    List<String> hashMultiGet(String key,String ...hashKeys);
+
+    /**
+     * 对hash值进行加法运算
+     * @param key
+     * @param hashKey
+     * @param delta
+     * @return
+     */
+    Long hashIncrement(String key,String hashKey,long delta);
+
+    /**
+     * 统计指定key的hash空间大小
+     * @param key
+     * @return
+     */
+    Long hashSize(String key);
+
+    /**
+     * 对指定key的指定hashKey进行设置
+     * @param key
+     * @param hashKey
+     * @param value
+     */
+    void hashPut(String key,String hashKey,Object value);
+
+    /**
+     * 对指定key进行批量操作
+     * @param key
+     * @param map
+     */
+    void hashPutAll(String key, Map<String,Object> map);
+
+    /**
+     * 根据Key获取所有Hash值并序列化返回
+     * @param key
+     * @param valueType
+     * @return
+     */
+    <T> Map<String,T> hashEntries(String key,Class<T> valueType);
+
+    /**
+     * 根据key获取所有Hash值
+     * @param key
+     * @return
+     */
+    Map<String,String> hashEntries(String key);
+
+    /**
+     * 获取list内范围值，并反序列化返回
+     * @param key
+     * @param start
+     * @param end
+     * @param valueType
+     * @return
+     */
+    <T> List<T> listRange(String key, long start, long end,Class<T> valueType);
+
+    /**
+     * 获取list内范围值，并返回
+     * @param key
+     * @param start
+     * @param end
+     * @return
+     */
+    List<String> listRange(String key,long start, long end);
+
+    /**
+     * 删除list首尾，只保留 [start, end] 之间的值
+     * @param key
+     * @param start
+     * @param end
+     */
+    void listTrim(String key, long start, long end);
+
+    /**
+     * 获取指定key的大小
+     * @param key
+     * @return
+     */
+    Long listSize(String key);
+
+    /**
+     * 向左push
+     * @param key
+     * @param object
+     */
+    void listLeftPush(String key,Object object);
+
+    /**
+     * 向左批量push
+     * @param key
+     * @param objects
+     */
+    void listLeftPushAll(String key,Object ...objects);
+
+    /**
+     * 向右push
+     * @param key
+     * @param object
+     */
+    void listRightPush(String key,Object object);
+
+    /**
+     * 批量向右push
+     * @param key
+     * @param objects
+     */
+    void listRightPushAll(String key,Object ...objects);
+
+    /**
+     * 指定插入在指定索引
+     * @param key
+     * @param index
+     * @param value
+     */
+    void listSet(String key,long index, Object value);
+
+    /**
+     * 删除list上的指定索引
+     * @param key
+     * @param index
+     */
+    void listRemove(String key,long index);
+
+    /**
+     * 获取指定key的指定索引的值
+     * @param key
+     * @param index
+     * @return
+     */
+    String listIndex(String key,long index);
+
+    /**
+     * 获取指定key的指定索引的值反序列化返回
+     * @param key
+     * @param index
+     * @param valueType
+     * @param <T>
+     * @return
+     */
+    <T> T listIndex(String key,long index,Class<T> valueType) throws IOException, JsonParseException;
+
+    /**
+     * 指定key进行左Pop操作
+     * @param key
+     * @return
+     */
+    String listLeftPop(String key);
+
+    /**
+     * 指定key进行左pop操作并反序列化返回
+     * @param key
+     * @param valueType
+     * @param <T>
+     * @return
+     */
+    <T> T listLeftPop(String key,Class<T> valueType)  throws IOException, JsonParseException;
+
+    /**
+     * 指定key进行右pop操作
+     * @param key
+     * @return
+     */
+    String listRightPop(String key);
+    /**
+     * 指定key进行右Pop操作
+     * @param key
+     * @param valueType
+     * @param <T>
+     * @return
+     * @throws IOException
+     * @throws JsonParseException
+     */
+    <T> T listRightPop(String key,Class<T> valueType)  throws IOException, JsonParseException;
 
 
 }
